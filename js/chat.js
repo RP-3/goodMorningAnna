784e11ffc41d0587fc5eef2976a4d9259e2837b7
Rohan <sarith21@gmail.com>
2015-01-17 16:37:58 -0800

    Adds firebase and hooks up chat client

--- js/chat.js
+++ js/chat.js
@@ -1,11 +1,58 @@
+var Firebase = require('firebase');
+var messageRef = new Firebase('https://goodmorninganna.firebaseIO.com/messages');
 
+matilda = messageRef;
 
 module.exports = function(){
+
+  //load up messages from messageRef
+  messageRef
+  .limitToLast(25)
+  .on('value', function(snapshot){
+    //empty the messagebox
+    $("#messagebox").empty();
+
+    var messageArray = [];
+    var data = snapshot.val();
+
+    for(var message in data){
+      messageArray.push(data[message]);
+    }
+
+    var formattedMessages = messageArray
+    .sort(function(a, b){
+      return a.time - b.time;
+    })
+    .map(function(element, index){
+      return "<p class='message'><strong>"+element.sender+": </strong>"+ element.text +"</p>";
+    });
+
+    $("#messagebox").append(formattedMessages);
+  });
   
   //add event listener to chatpane button
   $("#chatbutton").click(function(e){
     e.stopPropagation();
     $("#chatpane").animate({width:'toggle'},350);
   });
+
+  //add event listener for chat submission
+  $("#inputbox").keypress(function(e){
+    //if key was enter key
+    if(event.keyCode === 13){
+      e.preventDefault();
+      e.stopPropagation();
+      var message = e.target.textContent;
+      messageRef.push({
+        sender: 'anonymous',
+        text: message,
+        time: Date.now()
+      });
+      e.target.textContent = '';
+    }
+  });
+
+
+
 
 };
--- js/main.js
+++ js/main.js
@@ -4,24 +4,20 @@ Get name of user
 //TODO
 var userName = 'Anna';
 
-/*
-choose today's image and display it
-*/
-require('./backgroundImage.js')();
+$(document).on('ready', function(){
 
-/*
-Parse out today's time and greeting and display it
-*/
-require('./greeting.js')(userName);
+  /*choose today's image and display it*/
+  require('./backgroundImage.js')();
+
+  /*Parse out today's time and greeting and display it*/
+  require('./greeting.js')(userName);
 
 
-/*
-Fetch the location and weather and display it
-*/
-require('./weatherWidget.js')();
+  /*Fetch the location and weather and display it*/
+  require('./weatherWidget.js')();
 
 
-/*
-Set up firebase chat
-*/
-require('./chat.js')();
+  /*Set up firebase chat*/
+  require('./chat.js')();
+
+});
