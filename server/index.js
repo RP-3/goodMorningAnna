d0d6aa0252c663eb239d51a2af197b0b98682fdc
Rohan <sarith21@gmail.com>
2015-01-19 13:54:54 -0800

    Enables dev logging

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ var db = require('./config.js').db;
 var s3 = require('./config.js').s3;
 
 var express = require('express');
+var morgan = require('morgan');
 var app = express();
+
+//set up middleware stacl
+app.use(morgan('dev'));
+
 
 app.get('/', function(req, res){
   res.send('hello world');
